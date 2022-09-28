de4c471d5fd764ed80752f5ed7e46aec71f94183
Konrad Bauer <konradbauer94@gmail.com>
2022-09-29 00:26:23 +0200

    add paragraph on Label component

--- src/Label/index.js
+++ src/Label/index.js
@@ -1,14 +1,16 @@
 import "./style.css";
 
 const Label = () => (
-  <label>
-    <span className="form__labelText">Set currency:</span>
-    <select className="form__input" name="currency">
-      <option value="EUR">Euro</option>
-      <option value="USD">US Dollar</option>
-      <option value="RUB">Ruble</option>
-    </select>
-  </label>
+  <p>
+    <label>
+      <span className="form__labelText">Set currency:</span>
+      <select className="form__input" name="currency">
+        <option value="EUR">Euro</option>
+        <option value="USD">US Dollar</option>
+        <option value="RUB">Ruble</option>
+      </select>
+    </label>
+  </p>
 );
 
 export default Label;
