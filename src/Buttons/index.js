b97a1a58afbc42ef10b45970a7f50aee1e1ca809
Konrad Bauer <konradbauer94@gmail.com>
2022-09-28 22:45:37 +0200

    create Buttons component with styles

---
+++ src/Buttons/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+const Buttons = () => (
+  <p>
+    <button className="form__button">Calculate</button>
+    <button className="form__button" type="reset">
+      Reset
+    </button>
+  </p>
+);
+
+export default Buttons;
