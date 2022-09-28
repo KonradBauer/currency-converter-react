93ea7a8e9eab8fe818bc44874eff63722285b093
Konrad Bauer <konradbauer94@gmail.com>
2022-09-29 00:26:07 +0200

    add paragraph and placeholder custom in Input component

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,19 +1,21 @@
 import "./style.css";
 
-const Input = () => (
-  <label>
-    <span className="form__labelText">Amount*:</span>
-    <input
-      className="form__input"
-      min="1"
-      name="amount"
-      placeholder="set your PLN amount"
-      type="number"
-      step="any"
-      autofocus
-      required
-    />
-  </label>
+const Input = ({ placeholder }) => (
+  <p>
+    <label>
+      <span className="form__labelText">Amount*:</span>
+      <input
+        className="form__input"
+        min="1"
+        name="amount"
+        placeholder={placeholder}
+        type="number"
+        step="any"
+        autofocus
+        required
+      />
+    </label>
+  </p>
 );
 
 export default Input;
