758655ba9a5f9f4f83049d1c8e961d36355fa0ee
Konrad Bauer <konradbauer94@gmail.com>
2022-09-29 00:23:53 +0200

    modify css and added paragrap in Form component

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,5 +1,9 @@
 import "./style.css";
 
-const Form = ({ children }) => <div className="form">{children}</div>;
+const Form = ({ children }) => (
+  <div className="form">
+    <p>{children}</p>
+  </div>
+);
 
 export default Form;
