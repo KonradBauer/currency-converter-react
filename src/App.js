ffcd3319ac0216c12914ce6c7293884efe0e46d1
Konrad Bauer <konradbauer94@gmail.com>
2022-09-28 22:46:44 +0200

    add main body of app to app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Footer from "./Footer";
+import Buttons from "./Buttons";
+import Input from "./Input";
 
 function App() {
   return (
@@ -7,41 +9,20 @@ function App() {
         <form className="form">
           <fieldset className="form__fieldset">
             <legend className="form__legend">Currency converter</legend>
-            <p>
-              <label>
-                <span className="form__labelText">Amount*:</span>
-                <input
-                  className="form__input"
-                  min="1"
-                  name="amount"
-                  placeholder="set your PLN amount"
-                  type="number"
-                  step="any"
-                  autofocus
-                  required
-                />
-              </label>
-            </p>
-            <p>
-              <label>
-                <span className="form__labelText">Set currency:</span>
-                <select className="form__input" name="currency">
-                  <option value="EUR">Euro</option>
-                  <option value="USD">US Dollar</option>
-                  <option value="RUB">Ruble</option>
-                </select>
-              </label>
-            </p>
+            <Input />
+            <label>
+              <span className="form__labelText">Set currency:</span>
+              <select className="form__input" name="currency">
+                <option value="EUR">Euro</option>
+                <option value="USD">US Dollar</option>
+                <option value="RUB">Ruble</option>
+              </select>
+            </label>
           </fieldset>
           <section className="form__result">
             <span>Result:</span>
           </section>
-          <p>
-            <button className="form__button">Calculate</button>
-            <button className="form__button" type="reset">
-              Reset
-            </button>
-          </p>
+          <Buttons />
         </form>
         <Footer />
       </div>
