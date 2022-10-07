9f64873685bb826a8426986fab69210fbb0c150e
Konrad Bauer <konradbauer94@gmail.com>
2022-10-07 14:08:25 +0200

    added keys to currencies in currencies.js

--- src/currencies.js
+++ src/currencies.js
@@ -1,22 +1,28 @@
-let currencies = [
+const currencies = [
   {
+    key: EUR,
     currency: "Euro",
     shortcut: EUR,
     value: 4.87,
   },
   {
+    key: USD,
     currency: "American Dollar",
     shortcut: USD,
     value: 4.97,
   },
   {
+    key: RUB,
     currency: "Ruble",
     shortcut: RUB,
     value: 0.081,
   },
   {
-    currency: Franc,
+    key: CHF,
+    currency: "Franc",
     shortcut: CHF,
     value: 5.01,
   },
 ];
+
+export default currencies;
