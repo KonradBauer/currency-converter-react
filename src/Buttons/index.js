11d9666f685d38fbf9b47062622321f87bafac59
Konrad Bauer <konradbauer94@gmail.com>
2022-10-07 16:31:51 +0200

    added react fragment to Buttons component

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -3,16 +3,18 @@ import Form from "../Form";
 
 const Buttons = () => (
   <p>
-    <button
-      className="form__button"
-      onClick={(event) => event.preventDefault()}
-    >
-      Calculate
-    </button>
+    <>
+      <button
+        className="form__button"
+        onClick={(event) => event.preventDefault()}
+      >
+        Calculate
+      </button>
 
-    <button className="form__button" type="reset" onClick={() => Form.reset}>
-      Reset
-    </button>
+      <button className="form__button" type="reset" onClick={() => Form.reset}>
+        Reset
+      </button>
+    </>
   </p>
 );
 
