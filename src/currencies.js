7fca14b9a617552b0da2a67f7d999773733e871e
Konrad Bauer <konradbauer94@gmail.com>
2022-10-07 14:01:46 +0200

    added currencies.js - file with currencies array

---
+++ src/currencies.js
@@ -0,0 +1,22 @@
+let currencies = [
+  {
+    currency: "Euro",
+    shortcut: EUR,
+    value: 4.87,
+  },
+  {
+    currency: "American Dollar",
+    shortcut: USD,
+    value: 4.97,
+  },
+  {
+    currency: "Ruble",
+    shortcut: RUB,
+    value: 0.081,
+  },
+  {
+    currency: Franc,
+    shortcut: CHF,
+    value: 5.01,
+  },
+];
