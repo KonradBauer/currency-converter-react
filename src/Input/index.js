1c615ad388e432b42bb1cab166fb1f5665998866
Konrad Bauer <konradbauer94@gmail.com>
2022-10-14 13:22:04 +0200

    rename input value from input to number

--- src/Input/index.js
+++ src/Input/index.js
@@ -2,15 +2,15 @@ import "./style.css";
 import React, { useState } from "react";
 
 const Input = ({ placeholder, labelText }) => {
-  const [input, setInput] = useState("");
+  const [number, setNumber] = useState("");
 
   return (
     <p>
       <label>
         <span className="form__labelText">{labelText}</span>
         <input
-          value={input}
-          onChange={({ target }) => setInput(target.value)}
+          value={number}
+          onChange={({ target }) => setNumber(target.value)}
           className="form__input"
           min="1"
           name="amount"
