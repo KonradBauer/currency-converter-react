95d0348d4bc7787ec9e91df9b4d5adebdc1adcc0
Konrad Bauer <konradbauer94@gmail.com>
2022-10-14 13:17:17 +0200

    turn input to controlled component using useState hook

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,21 +1,28 @@
 import "./style.css";
+import React, { useState } from "react";
 
-const Input = ({ placeholder, labelText }) => (
-  <p>
-    <label>
-      <span className="form__labelText">{labelText}</span>
-      <input
-        className="form__input"
-        min="1"
-        name="amount"
-        placeholder={placeholder}
-        type="number"
-        step="any"
-        autofocus
-        required
-      />
-    </label>
-  </p>
-);
+const Input = ({ placeholder, labelText }) => {
+  const [input, setInput] = useState("");
+
+  return (
+    <p>
+      <label>
+        <span className="form__labelText">{labelText}</span>
+        <input
+          value={input}
+          onChange={({ target }) => setInput(target.value)}
+          className="form__input"
+          min="1"
+          name="amount"
+          placeholder={placeholder}
+          type="number"
+          step="any"
+          autofocus
+          required
+        />
+      </label>
+    </p>
+  );
+};
 
 export default Input;
