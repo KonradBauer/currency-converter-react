f068b28d7594613bf5fdd219fcb2e66e1a4f6ae4
Konrad Bauer <konradbauer94@gmail.com>
2022-10-18 21:20:31 +0200

    removed event.preventDefault from Buttons component

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -4,12 +4,7 @@ import Form from "../Form";
 const Buttons = () => (
   <p>
     <>
-      <button
-        className="form__button"
-        onClick={(event) => event.preventDefault()}
-      >
-        Calculate
-      </button>
+      <button className="form__button">Calculate</button>
 
       <button className="form__button" type="reset" onClick={() => Form.reset}>
         Reset
