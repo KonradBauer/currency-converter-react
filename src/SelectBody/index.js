62b4440bb1cc6eb68b58ee0e057ab4b3ac156536
Konrad Bauer <konradbauer94@gmail.com>
2022-10-18 22:04:37 +0200

    select render options from currencies.js added

--- src/SelectBody/index.js
+++ src/SelectBody/index.js
@@ -1,12 +1,12 @@
 import "./style.css";
-import "../currencies";
+import currencies from "../currencies";
 
-const SelectBody = ({ labelText, currencies }) => {
+const SelectBody = ({ labelText }) => {
   return (
     <p>
       <label>
         <span className="form__labelText">{labelText}</span>
-        <select className="form__input" name="currency">
+        <select className="form__input">
           {currencies.map((currency) => (
             <option>{currency.name}</option>
           ))}
