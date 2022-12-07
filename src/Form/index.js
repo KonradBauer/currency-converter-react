aac5ee0529f78b306dd5b123f0d60dc981189f30
Konrad Bauer <konradbauer94@gmail.com>
2022-12-07 01:41:34 +0100

    code refactor in Format ocmponent

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,11 @@
+import { calculateNewValue } from "@testing-library/user-event/dist/utils";
 import "./style.css";
 
-const onFormSubmit = (event) => {
-  event.preventDefault();
-};
-
 const Form = ({ children }) => {
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <form onSubmit={onFormSubmit} className="form">
       {children}
