8521b0e57277e85f2a6709d2a8ca4d9f286dc85e
Konrad Bauer <konradbauer94@gmail.com>
2022-12-07 01:39:48 +0100

    create controlled component in Input component

--- src/Input/index.js
+++ src/Input/index.js
@@ -2,15 +2,15 @@ import "./style.css";
 import React, { useState } from "react";
 
 const Input = ({ placeholder, labelText }) => {
-  const [number, setNumber] = useState("");
+  const [amount, setAmount] = useState("");
 
   return (
     <p>
       <label>
         <span className="form__labelText">{labelText}</span>
         <input
-          value={number}
-          onChange={({ target }) => setNumber(target.value)}
+          value={amount}
+          onChange={({ target }) => setAmount(target.value)}
           className="form__input"
           min="1"
           name="amount"
