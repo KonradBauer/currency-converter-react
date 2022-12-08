73e6a191855092f78b4a59b32dc2443108f02771
Konrad Bauer <konradbauer94@gmail.com>
2022-12-08 13:17:17 +0100

    added controlled select component

--- src/SelectBody/index.js
+++ src/SelectBody/index.js
@@ -1,12 +1,21 @@
+import React, { useState } from "react";
 import "./style.css";
 import currencies from "../currencies";
 
 const SelectBody = ({ labelText }) => {
+  const [currency, setCurrency] = useState("");
+
   return (
     <p>
       <label>
         <span className="form__labelText">{labelText}</span>
-        <select className="form__input">
+        <select
+          className="form__input"
+          value={currency}
+          onChange={({ target }) => {
+            setCurrency(target.value);
+          }}
+        >
           {currencies.map((currency) => (
             <option key={currency.key}>{currency.name}</option>
           ))}
