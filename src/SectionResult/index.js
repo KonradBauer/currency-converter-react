d2ee70d800af6b70d24aadd06ca619fbdda64238
Konrad Bauer <konradbauer94@gmail.com>
2022-12-08 13:16:52 +0100

    delete imports from SectionResult

--- src/SectionResult/index.js
+++ src/SectionResult/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import React from "react";
 
-const SectionResult = ({ children, text }) => {
+const SectionResult = () => {
   return (
     <section className="form__result">
       <span>Result:</span>
