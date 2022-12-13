7adeb6ea21eb787538e75e4c9e8e8e85ea6c5ddf
Konrad Bauer <konradbauer94@gmail.com>
2022-12-13 13:56:39 +0100

    update SectionResult component

--- src/SectionResult/index.js
+++ src/SectionResult/index.js
@@ -1,11 +1,16 @@
 import "./style.css";
 import React from "react";
 
-const SectionResult = ({ result }) => {
+const SectionResult = ({ resultData }) => {
+  const { result, amount, currency } = resultData;
+
   return (
     <section className="form__result">
       <span>
-        Result: <span className="section__fontStyle">{result}</span>
+        Result:{" "}
+        <span className="section__fontStyle">
+          {result && `${amount} PLN = ${result} ${currency}`}
+        </span>
       </span>
     </section>
   );
