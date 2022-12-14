aeec3ade72694d133fb7d33acf37476e34c284e8
Konrad Bauer <konradbauer94@gmail.com>
2022-12-14 12:34:40 +0100

    remove ReactFragment from Buttons component

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,20 +2,18 @@ import "./style.css";
 
 const Buttons = ({ setResult, setAmount }) => (
   <p>
-    <>
-      <button className="form__button">Calculate</button>
+    <button className="form__button">Calculate</button>
 
-      <button
-        className="form__button"
-        type="reset"
-        onClick={() => {
-          setResult("");
-          setAmount("");
-        }}
-      >
-        Reset
-      </button>
-    </>
+    <button
+      className="form__button"
+      type="reset"
+      onClick={() => {
+        setResult("");
+        setAmount("");
+      }}
+    >
+      Reset
+    </button>
   </p>
 );
 
