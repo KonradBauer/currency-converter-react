918d33fb0ab66c3b1ba39942421cae0077a659bb
Konrad Bauer <konradbauer94@gmail.com>
2023-01-04 23:11:33 +0100

    add return ratesData

--- src/useCurrencies.js
+++ src/useCurrencies.js
@@ -3,14 +3,18 @@ import { useState, useEffect } from "react";
 
 const useCurrencies = () => {
   const [ratesData, setRatesData] = useState({});
+  const getURL = "https://api.exchangerate.host/latest";
 
   useEffect = () => {
     axios
-      .get("https://api.exchangerate.host/latest")
+      .get(getURL)
       .then((response) => console.log(response.data))
       .catch((error) => console.error(error));
+
+    setRatesData(() => response.data);
   };
-  setRatesData(response.data);
+
+  return ratesData;
 };
 
 export default useCurrencies;
