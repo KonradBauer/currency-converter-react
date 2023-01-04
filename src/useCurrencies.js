64136a81d161befe938f1c644d8cb84f126a00c0
Konrad Bauer <konradbauer94@gmail.com>
2023-01-04 22:29:45 +0100

    added setRatesData

--- src/useCurrencies.js
+++ src/useCurrencies.js
@@ -4,18 +4,13 @@ import { useState, useEffect } from "react";
 const useCurrencies = () => {
   const [ratesData, setRatesData] = useState({});
 
-  useEffect = axios
-    .get("https://api.exchangerate.host/latest")
-    .then((response) => console.log(response.data))
-    .catch((error) => console.error(error));
-
-  const getData = () => {
-    setRatesData({
-      base,
-      date,
-      rates,
-    });
+  useEffect = () => {
+    axios
+      .get("https://api.exchangerate.host/latest")
+      .then((response) => console.log(response.data))
+      .catch((error) => console.error(error));
   };
+  setRatesData(response.data);
 };
 
 export default useCurrencies;
